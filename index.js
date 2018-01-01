409d01ea47a071be11788cf024d87e6f78591b86
Maythee Anegboonlap <null@llun.me>
2018-01-01 16:30:59 +0800

    Add progress information

--- index.js
+++ index.js
@@ -31,12 +31,28 @@ const logs = _.flatten(
       .map(item => path.join(__dirname, ROOT_LOGS_DIR, logDir, item))
   )
 )
-logs.forEach(item => {
-  parser(item).pipe(
-    through2(function(line, enc, callback) {
-      const data = JSON.parse(line.toString())
-      store.save(data)
-      callback()
+async function process(logs) {
+  for (let log of logs) {
+    await new Promise(resolve => {
+      console.log(log)
+      parser(log)
+        .pipe(
+          through2(function(line, enc, callback) {
+            const data = JSON.parse(line.toString())
+            store.save(data)
+            callback()
+          })
+        )
+        .on('error', e => {
+          console.error('error', e)
+        })
+        .on('finish', () => {
+          resolve()
+        })
     })
-  )
-})
+  }
+}
+
+process(logs)
+  .then(() => console.log('done'))
+  .catch(error => console.error(error))
